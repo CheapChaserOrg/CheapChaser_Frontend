6c22028f91ea4ab726e859c69e24f30ed9dc449e
Naduni Peiris <peirisnaduni13@gmail.com>
2025-03-02 22:15:45 +0530

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -9,7 +9,7 @@ import { Input } from "../components/ui/input";
 import { Textarea } from "../components/ui/textarea";
 import { Button } from "../components/ui/button";
 import { useToast } from "../components/ui/use-toast";
-
+//email connected to the contact us form
 const Contact = () => {
   const { toast } = useToast();
 <<<<<<< Updated upstream
