a4c7bbfbb88dc287aebcf1626c1d8d811f6a87f0
Kisii03 <venuri.20232234@iit.ac.lk>
2025-03-02 22:44:18 +0530

    Bookings12

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -10,6 +10,19 @@ const NotFound = () => {
       location.pathname
     );
   }, [location.pathname]);
+
+  return (
+    <div className="min-h-screen relative">
+      {/* Background Image with Overlay */}
+      <div 
+        className="absolute inset-0 bg-cover bg-center z-0"
+        style={{
+          backgroundImage: 'url("https://images.unsplash.com/photo-1469041797191-50ace28483c3?ixlib=rb-1.2.1&auto=format&fit=crop&w=2560&q=80")',
+        }}
+      >
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
