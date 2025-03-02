d532a3c76b21ee1b2604f5ff10cb6d1e505d06a5
Kisii03 <venuri.20232234@iit.ac.lk>
2025-03-02 22:14:31 +0530

    Bookings8

--- src/pages/Feedback.tsx
+++ src/pages/Feedback.tsx
@@ -4,7 +4,16 @@ const Feedback = () => {
   return (
     <div className="min-h-screen relative">
       {/* Background Image with Overlay */}
-      
+      <div 
+        className="absolute inset-0 bg-cover bg-center z-0"
+        style={{
+          backgroundImage: 'url("https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&auto=format&fit=crop&w=2560&q=80")',
+        }}
+      >
+      </div>
+
+      {/* Content */}
+    
     </div>
   );
 };
