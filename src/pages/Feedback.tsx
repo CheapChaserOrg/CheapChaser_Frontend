2a092cdef9b8dee7a4544a9a50182a13b189079e
Kisii03 <venuri.20232234@iit.ac.lk>
2025-03-02 22:17:40 +0530

    Bookings9
    Updated Feedback.tsx

--- src/pages/Feedback.tsx
+++ src/pages/Feedback.tsx
@@ -10,10 +10,12 @@ const Feedback = () => {
           backgroundImage: 'url("https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&auto=format&fit=crop&w=2560&q=80")',
         }}
       >
+        <div className="absolute inset-0 bg-black bg-opacity-50" />
       </div>
 
       {/* Content */}
-    
+      <div className="relative z-10 container mx-auto px-4 py-8">
+      </div>
     </div>
   );
 };
