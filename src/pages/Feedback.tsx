92f80a19331464d81b533a4db55fd61c07104255
Kisii03 <venuri.20232234@iit.ac.lk>
2025-03-02 22:12:57 +0530

    Bookings7

---
+++ src/pages/Feedback.tsx
@@ -0,0 +1,12 @@
+import FeedbackForm from "@/components/feedback/FeedbackForm";
+
+const Feedback = () => {
+  return (
+    <div className="min-h-screen relative">
+      {/* Background Image with Overlay */}
+      
+    </div>
+  );
+};
+
+export default Feedback;
