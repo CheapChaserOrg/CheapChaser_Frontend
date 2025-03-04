1dd672fcd5877224d02e5d0fe24abfb291ec151c
Sinega <w2082106@westminster.ac.uk>
2025-03-04 11:14:32 +0530

    Tour lanner.jsx

---
+++ src/pages/TourPlanner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TourPlanner() {
+    return (
+        <div>
+            <h2>Tour Planner</h2>
+            <p>Customize your trip based on your preferences and budget!</p>
+        </div>
+    );
+}
+
+export default TourPlanner;
