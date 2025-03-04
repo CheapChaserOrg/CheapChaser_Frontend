41ef016281454d4f989bec807626f855ea217ff6
Sinega <w2082106@westminster.ac.uk>
2025-03-04 10:47:10 +0530

    ServiceProvider

---
+++ src/pages/ServiceProvider.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ServiceProvider() {
+    return (
+        <div>
+            <h2>Service Provider</h2>
+            <p>Manage your services and reach more tourists!</p>
+        </div>
+    );
+}
+
+export default ServiceProvider;
