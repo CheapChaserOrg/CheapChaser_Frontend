593feac7586efaf97d52f7bf3a9210c6d4055b8e
Sinega <w2082106@westminster.ac.uk>
2025-03-27 19:09:37 +0530

    PaymentRequest.java

---
+++ src/main/java/com/cheapchaser/model/PaymentRequest.java
@@ -0,0 +1,28 @@
+package com.cheapchaser.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "payments")
+public class PaymentRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private double amount;
+    private String currency;
+    private String orderId;
+    private String returnUrl;
+    private String cancelUrl;
+    private String notifyUrl;
+}
