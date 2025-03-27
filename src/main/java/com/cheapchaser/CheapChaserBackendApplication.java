97e78206e2e41e27fdfff2e599dcf1601afde2ce
Sinega <w2082106@westminster.ac.uk>
2025-03-27 19:07:56 +0530

    Cheap ChaserBackendApplication.java

---
+++ src/main/java/com/cheapchaser/CheapChaserBackendApplication.java
@@ -0,0 +1,11 @@
+package com.cheapchaser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheapChaserBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CheapChaserBackendApplication.class, args);
+	}
+}
